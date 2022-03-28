701da9955fe2c88ecf62c7bc05a1e18dccb2c197
vira2121 <virajadhav2121@gmail.com>
2022-03-28 16:18:06 +0530

    done successfully

--- assign.py
+++ assign.py
@@ -15,19 +15,19 @@ if __name__=="__main__":
         c = int(input("main menu \n 1) Find Area of rectangle\n 2) Find perimeter of Rectangle \n 3) Find Area of Square\n 4) Exit"))
 
         if c==1:
-            num1 = int(input("Enter the length of rectangle: "))
-            num2 = int(input("Enter the breadth of rectangle: "))
-            result = rectangle(num1,num2)
+            length = int(input("Enter the length of rectangle: "))
+            breadth = int(input("Enter the breadth of rectangle: "))
+            result = rectangle(length,breadth)
             print(result)
 
         elif c==2:
-            num1 = int(input("Enter the length of rectangle: "))
-            num2 = int(input("Enter the breadth of rectangle: "))
-            result1 = perimeter_rect(num1,num2)
+            length = int(input("Enter the length of rectangle: "))
+            breadth = int(input("Enter the breadth of rectangle: "))
+            result1 = perimeter_rect(length,breadth)
             print(result1)
         elif c==3:
-            num1 = int(input("Enter side of square: "))
-            result2 = area_square(num1)
+            side = int(input("Enter side of square: "))
+            result2 = area_square(side)
             print(result2)
         else:
             check = 0
