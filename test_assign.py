5436ba432d02343be85d6ba05e307462ceb52c94
vira2121 <virajadhav2121@gmail.com>
2022-03-28 16:42:31 +0530

    updated

--- test_assign.py
+++ test_assign.py
@@ -1,17 +1,21 @@
 import assign
+import pytest
 
+@pytest.mark.skip(reason = "currently no need to execute")
 def test_rect():
     x=5
     y=7
     result=assign.rectangle(x,y)
     assert x*y==result
 
+@pytest.mark.myproject
 def test_periRec():
     x=5
     y=8
     result=assign.perimeter_rect(x,y)
     assert 2*(x+y)==result
 
+@pytest.mark.myproject
 def test_areaSquare():
     x=8
     result=assign.area_square(x)
